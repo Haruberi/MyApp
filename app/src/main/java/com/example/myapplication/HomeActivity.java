774324d8b3d5d3e81477b8183d4aa22e41cb19e3
Anna <anna.h.hallberg.92@gmail.com>
2021-03-19 13:11:02 +0100

    Add classes for tabs

---
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -0,0 +1,6 @@
+package com.example.myapplication;
+
+import androidx.fragment.app.Fragment;
+
+public class HomeActivity extends Fragment {
+}
--- app/src/main/java/com/example/myapplication/StartTabbedActivity.java
+++ app/src/main/java/com/example/myapplication/StartTabbedActivity.java
@@ -1,39 +1,38 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
-
-import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
-import com.example.myapplication.ui.main.SectionsPagerAdapter;
-
-public class StartTabbedActivity extends AppCompatActivity {
-
+public class StartTabbedActivity extends Fragment {
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_tabbed);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
+
+
+
+    //@Override
+    //protected void onCreate(Bundle savedInstanceState) {
+      //  super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_start_tabbed);
+        //SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        //ViewPager viewPager = findViewById(R.id.view_pager);
+        //viewPager.setAdapter(sectionsPagerAdapter);
+        //TabLayout tabs = findViewById(R.id.tabs);
+        //tabs.setupWithViewPager(viewPager);
+        //FloatingActionButton fab = findViewById(R.id.fab);
+//
+  //      fab.setOnClickListener(new View.OnClickListener() {
+    //        @Override
+      //      public void onClick(View view) {
+        //        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+          //              .setAction("Action", null).show();
+            //}
+        //});
