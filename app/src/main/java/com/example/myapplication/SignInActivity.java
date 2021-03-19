afd8073a303a2adcf0baeaa7b4fbab7285c36b1c
Anna <anna.h.hallberg.92@gmail.com>
2021-03-19 20:44:57 +0100

    Work well, next step: sqlite

--- app/src/main/java/com/example/myapplication/SignInActivity.java
+++ app/src/main/java/com/example/myapplication/SignInActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,4 +9,9 @@ import android.view.View;
 import android.widget.Button;
 
 public class SignInActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
     }
+}
