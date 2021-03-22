8cbcffc289a5127b789b660ccc403dbed9d7d0d0
Anna <anna.h.hallberg.92@gmail.com>
2021-03-22 22:13:43 +0100

    Almost complete, something not working when synchronising login and reg

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -1,6 +1,24 @@
 package com.example.myapplication;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-public class HomeActivity extends Fragment {
+public class HomeActivity extends AppCompatActivity {
+
+    TextView nameText;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_layout);
+        nameText=(TextView)findViewById(R.id.nameUserText);
+
+        Bundle b=getIntent().getExtras();
+        String username=b.getString("username");
+        nameText.setText(username);
+
+    }
 }
--- app/src/main/java/com/example/myapplication/SignInActivity.java
+++ app/src/main/java/com/example/myapplication/SignInActivity.java
@@ -3,26 +3,50 @@ package com.example.myapplication;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignInActivity extends AppCompatActivity {
 
-    EditText username, password;
+    EditText username1, password1;
     static LanguageDB languageDB;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-        username = (EditText) findViewById(R.id.signInPageUser);
-        password = (EditText) findViewById(R.id.signInPagePass);
+        username1 = (EditText) findViewById(R.id.signInPageUser);
+        password1 = (EditText) findViewById(R.id.signInPagePass);
         languageDB = new LanguageDB(this);
     }
 
     //logic for sign in
-    public void signIn(View v){
+    public void signIn(View v) {
+        String username = username1.getText().toString();
+        String password = password1.getText().toString();
+        Cursor c = SignInActivity.languageDB.userLoginCheck(username);
+        c.moveToFirst();
 
+        if (c == null) {
+            Toast.makeText(SignInActivity.this, "Invalid credentials for " + username, Toast.LENGTH_LONG).show();
+            username1.setText("");
+            password1.setText("");
+        } else {
+            String username2 = c.getString(0);
+            String password2 = c.getString(1);
+            if (password1.equals(password2)) {
+                Intent i = new Intent(SignInActivity.this, HomeActivity.class);
+                i.putExtra("username", username2);
+                startActivity(i);
+            } else{
+                Toast.makeText(SignInActivity.this,"Invalid credentials for user "+username,Toast.LENGTH_LONG).show();
+            }
+            username1.setText("");
+            password1.setText("");
+        }
     }
 }
