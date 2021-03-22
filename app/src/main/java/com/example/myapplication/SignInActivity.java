d3f17f7c1bffb9e5d150351baa949839f5170af5
Anna <anna.h.hallberg.92@gmail.com>
2021-03-22 13:10:07 +0100

    Add edittext from signinactivity in class

--- app/src/main/java/com/example/myapplication/SignInActivity.java
+++ app/src/main/java/com/example/myapplication/SignInActivity.java
@@ -7,11 +7,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class SignInActivity extends AppCompatActivity {
+
+    EditText username,password;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        username=(EditText)findViewById(R.id.editTextSigninUser);
+        password=(EditText)findViewById(R.id.editTextSigninPassword);
     }
 }
