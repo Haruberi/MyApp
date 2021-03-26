2780cc5874c662332b1b9d1817732debab971af0
Anna <anna.h.hallberg.92@gmail.com>
2021-03-26 08:55:00 +0100

    Back to where there's a tabbar

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -1,35 +1,37 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
-public class HomeActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class HomeActivity extends AppCompatActivity {
+    //Initialize variable
     TextView nameText;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_layout);
 
-        nameText=(TextView)findViewById(R.id.nameUserText);
+        //for getting Name of user
+        nameText = (TextView) findViewById(R.id.nameUserText);
 
-        Bundle b=getIntent().getExtras();
-        String name=b.getString("name");
+        Bundle b = getIntent().getExtras();
+        String name = b.getString("name");
         nameText.setText(name);
-
-        TabLayout tabLayout=findViewById(R.id.tabLayout);
-        TabItem learnTab=findViewById(R.id.learnTab);
-        TabItem homeTab=findViewById(R.id.homeTab);
-        TabItem faqTab=findViewById(R.id.faqTab);
-
-        PagerAdapter pagerAdapter=new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
-
     }
 }
