48310bc3d8cee0f2b87bf5eda45a33e3f0cb5be0
Anna <anna.h.hallberg.92@gmail.com>
2021-03-30 08:44:01 +0200

    Init

--- app/src/main/java/com/example/myapplication/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/HomeFragment.java
@@ -17,7 +17,9 @@ public class HomeFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_home,container,false);
     }
--- app/src/main/java/com/example/myapplication/ViewPagerAdapter.java
+++ app/src/main/java/com/example/myapplication/ViewPagerAdapter.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 public class ViewPagerAdapter extends FragmentPagerAdapter{
