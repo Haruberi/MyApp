a6cf27d3e7302f9cad7707e531c4acfaea3c64f7
Anna <anna.h.hallberg.92@gmail.com>
2021-04-08 11:26:00 +0200

    Add marginBottom and it works

--- app/src/main/java/com/example/myapplication/LearnFragment.java
+++ app/src/main/java/com/example/myapplication/LearnFragment.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,14 +11,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class LearnFragment extends Fragment {
+
+    Button chooseJpnBtn;
 
     public LearnFragment(){}
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Navigate to JpnActivity
+
+        //Navigate to ChiActivity
+
+        //Navigate to KorActivity
+
     }
 
     @Nullable
@@ -29,5 +40,7 @@ public class LearnFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_learn,
                 container,
                 false);
+
+
     }
 }
