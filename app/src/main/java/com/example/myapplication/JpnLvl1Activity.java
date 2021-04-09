d924959b9f168905e2dcbb12cca4d3b40c31c2df
Anna <anna.h.hallberg.92@gmail.com>
2021-04-09 17:15:16 +0200

    Add QuizActivity to start creating quizes!

--- app/src/main/java/com/example/myapplication/JpnLvl1Activity.java
+++ app/src/main/java/com/example/myapplication/JpnLvl1Activity.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class JpnLvl1Activity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class JpnLvl1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jpn_lvl1);
+
+        Button buttonStart=findViewById(R.id.btnStart);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startQuiz();
+            }
+        });
+    }
+
+    //here open our second activity quizactivity
+    private void startQuiz(){
+        Intent intent=new Intent(JpnLvl1Activity.this,QuizActivity.class);
+        startActivity(intent);
     }
 }
