970744907c830d648299a95afa35ca13d2f01bdb
Anna <anna.h.hallberg.92@gmail.com>
2021-04-12 09:02:31 +0200

    Create QuizContract class for column and table names

--- app/src/main/java/com/example/myapplication/QuizContract.java
+++ app/src/main/java/com/example/myapplication/QuizContract.java
@@ -1,4 +1,23 @@
 package com.example.myapplication;
 
-public class QuizContract {
+import android.provider.BaseColumns;
+
+public final class QuizContract implements BaseColumns {
+
+    private QuizContract(){}
+
+    public static class WordTable {
+        public static final String TABLE_NAME="quiz_words";
+        public static final String COLUMN_WORD="word";
+
+        public static final String COLUMN_SENTENCE="word_sentence";
+        public static final String COLUMN_SENTENCE_TRANSLATION="sentence_translation";
+
+        public static final String COLUMN_OPTION1="option1";
+        public static final String COLUMN_OPTION2="option2";
+        public static final String COLUMN_OPTION3="option3";
+        public static final String COLUMN_ANSWER_NR="answer_nr";
+
+
+    }
 }
