59329fa28315d99a8df4e4d45b29c8f126a5c8ae
Anna <anna.h.hallberg.92@gmail.com>
2021-04-26 12:06:03 +0200

    Add chinese table for quiz

--- app/src/main/java/com/example/myapplication/QuizDatabase/QuizContract.java
+++ app/src/main/java/com/example/myapplication/QuizDatabase/QuizContract.java
@@ -7,38 +7,37 @@ public final class QuizContract implements BaseColumns {
     private QuizContract() {
     }
 
+    //JpnTable
     public static class JpnWordTable{
         public static final String _ID = "_id";
-    public static final String TABLE_NAME="jpn_word_table";
-    public static final String COLUMN_WORD="jpn_word";
-    public static final String COLUMN_SENTENCE="jpn_sentence";
-    public static final String COLUMN_SENTENCE_TRANSLATION="jpn_sentence_translation";
+        public static final String TABLE_NAME="jpn_word_table";
+        public static final String COLUMN_WORD="jpn_word";
+        public static final String COLUMN_SENTENCE="jpn_sentence";
+        public static final String COLUMN_SENTENCE_TRANSLATION="jpn_sentence_translation";
 
-    public static final String COLUMN_OPTION_1="option_1";
-    public static final String COLUMN_OPTION_2="option_2";
-    public static final String COLUMN_OPTION_3="option_3";
-    public static final String COLUMN_ANSWER_NR="answer_nr";
+        public static final String COLUMN_OPTION_1="option_1";
+        public static final String COLUMN_OPTION_2="option_2";
+        public static final String COLUMN_OPTION_3="option_3";
+        public static final String COLUMN_ANSWER_NR="answer_nr";
     }
-}
-
 
     //ChiTable
 
-    //ublic static class ChiWordTable {
-    //        public static final String TABLE_NAME = "ChiQuizWords";
-    //
-    //        public static final String _ID = "ID";
-    //        public static String COLUMN_WORD = "word";
-    //        public static String COLUMN_SENTENCE = "sentence";
-    //        public static String COLUMN_SENTENCE_TRANSLATION = "translation";
-    //
-    //        public static String COLUMN_OPTION1 = "option1";
-    //        public static String COLUMN_OPTION2 = "option2";
-    //        public static String COLUMN_OPTION3 = "option3";
-    //        public static String COLUMN_ANSWER_NR = "answer_nr";
-    //    }
-    //
-    //
+    public static class ChiWordTable {
+            public static final String _ID = "_id";
+            public static final String TABLE_NAME = "chi_word_table";
+            public static String COLUMN_WORD = "chi_word";
+            public static String COLUMN_SENTENCE = "chi_sentence";
+            public static String COLUMN_SENTENCE_TRANSLATION = "chi_sentence_translation";
+
+            public static String COLUMN_OPTION1 = "option1";
+            public static String COLUMN_OPTION2 = "option2";
+            public static String COLUMN_OPTION3 = "option3";
+            public static String COLUMN_ANSWER_NR = "answer_nr";
+        }
+}
+
+
     //    //JpnTable
     //    public static class JpnWordTable {
     //
